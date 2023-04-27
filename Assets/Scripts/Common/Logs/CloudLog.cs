3a6d6869dd32397794b2153432f27c1057e58fb1
云是随机的 <56253496+3436826031@users.noreply.github.com>
2023-04-27 11:29:03 +0800

    日志管理类

---
+++ Assets/Scripts/Common/Logs/CloudLog.cs
@@ -0,0 +1,30 @@
+
+
+
+
+namespace CloudX{
+
+
+
+
+
+//自定义的Debug日志管理
+    public class CloudLog{
+
+
+        public static void Log(){   
+
+            print();
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
